74dd89764900c249d11b85b4292ad89314a94239
Leo Kim <me@leok.kr>
2022-07-27 00:33:02 +0900

    code format

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 # from .some import thing
 from .user import User, UserRole
+
 __all__ = [User, UserRole]
--- routes/v1/tests.py
+++ routes/v1/tests.py
@@ -5,8 +5,11 @@ from models import User, UserRole
 
 test_router = APIRouter()
 
+
 @test_router.get("/{id}", response_model=User)
-async def echo_data(id: str, name: str, role: UserRole = Query(UserRole.patient, description="유저 타입")):
-    obj={"id":id, "role":role, "name":name}
+async def echo_data(
+    id: str, name: str, role: UserRole = Query(UserRole.patient, description="유저 타입")
+):
+    obj = {"id": id, "role": role, "name": name}
     print(obj)
     return User.parse_obj(obj)
