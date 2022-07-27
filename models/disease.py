f136f868a0454b23f211e498411f134a40e4bee0
Leo Kim <me@leok.kr>
2022-07-27 23:18:39 +0900

    Fix Typo

--- models/disease.py
+++ models/disease.py
@@ -10,10 +10,10 @@ class Disease(BaseModel):
     subname: str = Field(
         ..., description="질병의 풀네임", example="4q21 microdeletion syndrome"
     )
-    symptoms: List(str) = Field(
+    symptoms: List[str] = Field(
         ..., description="질병의 증상들", example="List['과도한 웃음', '발작']"
     )
-    affected: List(str) = Field(
+    affected: List[str] = Field(
         ..., description="질병에 의해 영향 받는 곳", example="List['신경', '근육']"
     )
     supported: bool = Field(..., description="의료비 지원 여부")
