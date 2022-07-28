648649d86e9acb97c280cd34eb527760963783af
Leo Kim <me@leok.kr>
2022-07-29 02:49:40 +0900

    feat: query benefits that user requests

--- routes/v1/benefits.py
+++ routes/v1/benefits.py
@@ -1,3 +1,4 @@
+from pydoc import doc
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
@@ -21,3 +22,10 @@ async def request(disease: Disease,user: str = Depends(verify_token)):
     benefit = Benefit(status=BenefitStatus.waiting, disease=disease, userId=user.id)
     await db.insert_one("benefits", benefit.dict())
     return benefit
+
+@benefit_router.get("/{user}", response_model=List[Benefit], description="특정 환자가 신청한 대기중인 산정특례를 반환합니다. (의사만 접근 가능합니다)")
+async def find_benefit(user:str, requester: str = Depends(verify_doctor)):
+    user: Patient = await db.get_user(user)
+    if user.isDoctor:
+        raise HTTPException(status_code=403, detail="Should be Patient to query benefits")
+    return [Benefit.parse_obj(document) for document in await db.find_many("benefits", "userId", user.id)]
