2ba67919e4e5e1721bd7bbeaf9abbf97b31cd2b4
Leo Kim <me@leok.kr>
2022-07-28 18:20:27 +0900

    feat(auth): patient

--- routes/v1/tests.py
+++ routes/v1/tests.py
@@ -37,80 +37,68 @@ async def add_doctor(
 
 
 @test_router.post("/add_patient", response_model=Patient)
-async def add_patient(
-    symptoms: List[Symptom],
-    diseases: List[Disease],
-    id: str = Form(...),
-    name: str= Form(...),
-    phoneNumber: str= Form(...),
-    isDoctor: bool= Form(...),
-    healthInsuranceNumber: int= Form(...),
-    doctor: str= Form(...)
-):
-    pat = Patient(
-        id=id,
-        name=name,
-        phoneNumber=phoneNumber,
-        isDoctor=isDoctor,
-        healthInsuranceNumber=healthInsuranceNumber,
-        doctor=doctor,
-        symptoms=symptoms,
-        diseases=diseases
-    )
-    await db.insert_one("users", pat.dict())
-    return pat
-
-
-@test_router.get("/benifit_echo", response_model=Benefit)
-async def echo_benifit(
-    id: str, memo: str, date: date, type: DiseaseType, detail: str, signature: str
-):
-    obj = {
-        "id": id,
-        "memo": memo,
-        "date": date,
-        "type": type,
-        "detail": detail,
-        "signature": signature,
-    }
-    return Benefit.parse_obj(obj)
-
-
-@test_router.get("/disease_echo", response_model=Disease)
-async def echo_disease(
-    id: str,
-    name: str,
-    subname: str,
-    symptoms: List[str],
-    affected: List[str],
-    supported: bool,
-    required: bool,
-    code: str,
-):
-    obj = {
-        "id": id,
-        "name": name,
-        "subname": subname,
-        "symptoms": symptoms,
-        "affected": affected,
-        "supported": supported,
-        "required": required,
-        "code": code,
-    }
-    return Disease.parse_obj(obj)
-
-
-@test_router.get("/symptom_echo", response_model=Symptom)
-async def echo_symptom(name: str, date: date, time: time, symptoms: str):
-    obj = {
-        "name": name,
-        "date": date,
-        "time": time,
-        "symptoms": symptoms,
-    }
-    return Symptom.parse_obj(obj)
-
+async def add_patient(patient: Patient):
+    # pat = Patient(
+    #     id=id,
+    #     name=name,
+    #     phoneNumber=phoneNumber,
+    #     isDoctor=isDoctor,
+    #     healthInsuranceNumber=healthInsuranceNumber,
+    #     doctor=doctor,
+    #     symptoms=symptoms,
+    #     diseases=diseases
+    # )
+    await db.insert_one("users", patient.dict())
+    return patient
 
 @test_router.get("/@me", response_model=Union[Doctor, Patient], response_model_exclude_unset=True)
 async def get_me(id: str = Depends(verify_token)):
     return await db.get_user(id)
+
+
+# @test_router.get("/disease_echo", response_model=Disease)
+# async def echo_disease(
+#     id: str,
+#     name: str,
+#     subname: str,
+#     symptoms: List[str],
+#     affected: List[str],
+#     supported: bool,
+#     required: bool,
+#     code: str,
+# ):
+#     obj = {
+#         "id": id,
+#         "name": name,
+#         "subname": subname,
+#         "symptoms": symptoms,
+#         "affected": affected,
+#         "supported": supported,
+#         "required": required,
+#         "code": code,
+#     }
+#     return Disease.parse_obj(obj)
+
+# @test_router.get("/symptom_echo", response_model=Symptom)
+# async def echo_symptom(name: str, date: date, time: time, symptoms: str):
+#     obj = {
+#         "name": name,
+#         "date": date,
+#         "time": time,
+#         "symptoms": symptoms,
+#     }
+#     return Symptom.parse_obj(obj)
+
+# @test_router.get("/benifit_echo", response_model=Benefit)
+# async def echo_benifit(
+#     id: str, memo: str, date: date, type: DiseaseType, detail: str, signature: str
+# ):
+#     obj = {
+#         "id": id,
+#         "memo": memo,
+#         "date": date,
+#         "type": type,
+#         "detail": detail,
+#         "signature": signature,
+#     }
+#     return Benefit.parse_obj(obj)
